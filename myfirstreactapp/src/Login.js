34f10a2c36218a150b7eba263bf294b5633075e3
RebbniBasha <48984198+RebbniBasha@users.noreply.github.com>
2019-12-27 20:25:38 +0530

    Create Login.js

---
+++ myfirstreactapp/src/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Header'
+import Main from './Main'
+import Footer from './Footer'
+
+function Login(){
+
+    return(
+        <div>
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    )
+}
+
+export default Login
